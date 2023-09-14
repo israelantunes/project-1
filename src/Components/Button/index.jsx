d03a7f2c8a25abbc36fbdfcd89ba8f9a29487c68
Israel Antunes <israelantuunes@gmail.com>
2023-09-14 19:20:32 -0300

    First Commit

---
+++ src/Components/Button/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css'
+
+import { Component } from "react";
+
+export class Button extends Component {
+    render() {
+        const {text, actionFn, disabled} = this.props;
+        return(
+        <button 
+            className = 'button'
+            type      = 'button'
+            onClick   = {actionFn}
+            disabled  = {disabled}
+        >
+            {text}
+        </button>
+        )
+    }
+}
---
+++ src/Components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+export const PostCard = ({title, body, cover, id}) => (
+
+        <div className = 'post'>
+        <img src       = {cover} alt = {title} />
+        <div className = 'post-card'>
+            <h2> {title}</h2>
+            <p> {body}</p>
+          </div>
+          </div>
+    );
